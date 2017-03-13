57232293099ccabc7a2a8f9ecd46d4a9c10aca2e
Adonias Lopez <csuadonias@yahoo.com>
2017-03-13 11:37:16 -0700

    Manage Bean and Connection Pool

--- WokNTandoor/src/java/WokNTandoorDB.java
+++ WokNTandoor/src/java/WokNTandoorDB.java
@@ -7,18 +7,28 @@
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.sql.DataSource;
 
 /**
  *
  * @author Adonias
  */
+@ManagedBean
 @Named(value = "wokNTandoorDB")
 @SessionScoped
 public class WokNTandoorDB implements Serializable {
+    @Resource(name="wokntandoor")
+    private DataSource source;
     /**
      * Creates a new instance of WokNTandoorDB
      */
     public WokNTandoorDB() {
+    }
+    
+    public void getMenuItems(String subMenu){
+        
     }
     
 }
