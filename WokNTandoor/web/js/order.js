824f63f8994f48a50a13478fc7cef49076d08479
Adonias Lopez <csuadonias@yahoo.com>
2017-04-10 09:21:00 -0700

    order page awesomeness

--- WokNTandoor/web/js/order.js
+++ WokNTandoor/web/js/order.js
@@ -18,28 +18,44 @@ function openTab(evt, foodMenu) {
     evt.currentTarget.className += " w3-red";
 }
 
-function addSubtotal(item_id) {
-    var subtotal, price_in, quantity, quantity_in;
-
+function addSubtotal(item_id, cat_id) {
+    var subtotal, price_in, quantity, quantity_in, cat_subtotal;
     // Prevents decimal diarrhea
+    cat_subtotal = document.getElementById(cat_id);
+    cat_subtotal.style.display = "block";
     price_in = parseFloat(document.getElementById("p" + item_id).innerHTML);
+    cat_subtotal.innerHTML = (parseFloat(cat_subtotal.innerHTML) + price_in).toFixed(2);
     quantity = document.getElementById("q" + item_id);
     quantity_in = parseInt(quantity.innerHTML);
     subtotal = document.getElementById("subtotal");
     subtotal.innerHTML = (parseFloat(subtotal.innerHTML) + price_in).toFixed(2);
     quantity.innerHTML = quantity_in + 1;
+    var x = document.getElementById("orderButton");
+    if (x.style.display == 'none') {
+        x.style.display = 'inline';
+    }
 }
 
-function subtractSubtotal(item_id) {
-    var subtotal, price_in, quantity, quantity_in;
-
+function subtractSubtotal(item_id, cat_id) {
+    var subtotal, price_in, quantity, quantity_in, cat_subtotal;
     price_in = parseFloat(document.getElementById("p" + item_id).innerHTML);
     quantity = document.getElementById("q" + item_id);
+    cat_subtotal = document.getElementById(cat_id);
     quantity_in = parseInt(quantity.innerHTML);
     if (quantity_in > 0) {
+        if(parseFloat(cat_subtotal.innerHTML) > 0){
+            cat_subtotal.innerHTML = (parseFloat(cat_subtotal.innerHTML) - price_in).toFixed(2);
+        }
         subtotal = document.getElementById("subtotal");
         subtotal.innerHTML = (parseFloat(subtotal.innerHTML) - price_in).toFixed(2);
         quantity.innerHTML = quantity_in - 1;
+    }
+    if(parseFloat(cat_subtotal.innerHTML) <= 0){
+            cat_subtotal.style.display = "none";
+    }
+    var x = document.getElementById("orderButton");
+    if (parseFloat(subtotal.innerHTML) === 0) {
+        x.style.display = 'none';
     }
 }
 
@@ -50,4 +66,12 @@ function toggle(table_id){
     } else {
         x.style.display = 'none';
     }
+}
+function placeOrder(){
+    var quantity, item_id, subtotal;
+    item_id = 0;
+    subtotal = document.getElementById("subtotal");
+    subtotal = parseFloat(subtotal.innerHTML).toFixed(2);
+    quantity = document.getElementById("q" + item_id);
+       
 }
