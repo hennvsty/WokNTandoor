da4fded091ba615a27dc8c62e59737a5a51b0b91
Doan D <tamitystg@gmail.com>
2017-04-09 20:17:19 -0700

    Login

--- WokNTandoor/web/js/login.js
+++ WokNTandoor/web/js/login.js
@@ -11,6 +11,27 @@
 
 }).call(this);
 
+$(document).ready(function(){
+    $("#login").click(function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+        
+        if (username == '' || password == ''){
+            alert("Fields cannot be blank");
+        } else {
+            if (username == "NatashaB") {
+                if (password == "brahmbhatt") {
+                    window.location.replace("homepage.xhtml");
+                } else {
+                    alert("Invalid Password");
+                }
+            } else {
+                alert("Invalid UserID");
+            }
+        }
+    });
+});
+
 function pasuser(form) {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
