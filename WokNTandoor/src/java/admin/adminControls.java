db56cea285bfe196dead571b43fa35fd10d626ae
Adonias Lopez <csuadonias@yahoo.com>
2017-05-09 14:53:10 -0700

    Upload picture functionality works

--- WokNTandoor/src/java/admin/adminControls.java
+++ WokNTandoor/src/java/admin/adminControls.java
@@ -5,21 +5,31 @@
  */
 package admin;
 
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxAuthFinish;
+import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxEntry;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxWebAuthNoRedirect;
+import com.dropbox.core.DbxWriteMode;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.Serializable;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +38,9 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
 import javax.sql.DataSource;
+import org.apache.commons.io.IOUtils;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
@@ -39,7 +51,8 @@ import org.primefaces.model.UploadedFile;
 @ManagedBean(name = "adminControls")
 @SessionScoped
 public class adminControls implements Serializable{
-    @Resource(name="jdbc/wokntandoordb")
+    //@Resource(name="jdbc/wokntandoordb")
+    private static final String ACCESS_TOKEN = "HG3Mm8N5SwAAAAAAAAAAOTIb_WJWNTQZ7WYZRdLPiEZcg1mM8t11b_eOcC4OUhBm";
     private DataSource source; 
     private String outputMessage;
     private String action;
@@ -83,15 +96,37 @@ public class adminControls implements Serializable{
         this.uploadedPicture = uploadedPicture;
     }
     
-    public void handleFileUpload(FileUploadEvent event) {
+    public void handleFileUpload(FileUploadEvent event) throws IOException, DbxException{
+        DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
+            Locale.getDefault().toString());
+
+        DbxClient client = new DbxClient(config, ACCESS_TOKEN);
+
+        InputStream inputStream = null;
+        try {
+            UploadedFile uploadedfile = event.getFile();
+            inputStream = uploadedfile.getInputstream();
+            DbxEntry.File uploadedFile = client.uploadFile("/images/test.jpg",
+                DbxWriteMode.add(), uploadedfile.getSize(), inputStream);
+            String sharedUrl = client.createShareableUrl("/test.jpg");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(adminControls.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DbxException ex) {
+            Logger.getLogger(adminControls.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(adminControls.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            inputStream.close();
+        }
+        
+        /*
         UploadedFile uploadedFile = event.getFile();
-        String path = FacesContext.getCurrentInstance().getExternalContext()
-            .getRealPath("/images");
-        String fileName = uploadedFile.getFileName();
-        String contentTypes = uploadedFile.getContentType();
-        try (InputStream input = uploadedFile.getInputstream()){
-            //OutputStream out = new FileOutputStream(file);
-            File tempFile = new File("/images", event.getFile().getFileName());
+        InputStream input = uploadedFile.getInputstream();
+        OutputStream output = new FileOutputStream(new File("/images/","newPicture"));
+        try{
+            BufferedImage image = ImageIO.read(input);
+            ImageIO.write(image, "jpg", output);
+            IOUtils.copy(input, output);
             FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
@@ -99,6 +134,11 @@ public class adminControls implements Serializable{
             FacesMessage message = new FacesMessage(e.getMessage());
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
+        finally{
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(output);
+        }*/
+
         
     }
     
